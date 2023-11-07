39dda5e627a817045da62cd84898136bdbfc16ab
StevieBrooks <brookscodeblog@gmail.com>
2023-11-07 08:59:20 +0700

    write function to determine winner

--- src/components/GameCell.jsx
+++ src/components/GameCell.jsx
@@ -6,14 +6,28 @@ export default function GameCell( { index } ) {
 
     const [player, setPlayer, boardState, setBoardState, result, setResult] = useContext(GameState)
 
+    const winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
     const clickHandler = (e) => {
         const cellToAlter = e.target.classList[0]
         const updatedBoardState = [...boardState]
         updatedBoardState[cellToAlter] = player
         setBoardState(updatedBoardState)
-
         setPlayer(player === 1 ? 2 : 1)
-        console.log(player)
+        calculateWinner()
+    }
+
+    const calculateWinner = () => {
+        return winningCombos.forEach(item => {
+            const check1 = item[0]
+            const check2 = item[1]
+            const check3 = item[2]
+            if(boardState[check1] === 1 && boardState[check2] === 1 && boardState[check3] === 1) {
+                console.log("player 1 wins")
+            } else if(boardState[check1] === 2 && boardState[check2] === 2 && boardState[check3] === 2) {
+                console.log("player 2 wins")
+            }
+        })
     }
 
     return (
--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -1,9 +1,14 @@
+import { useContext } from "react"
+import { GameState } from "../ContextFile"
 export default function ScoreBoard() {
+
+    const [player, setPlayer, boardState, setBoardState, result, setResult] = useContext(GameState)
+
     return (
 
         <div className="scoreboard w-96 bg-slate-300 m-auto flex flex-wrap justify-around items-center">
-            <h3>P1: <span>00</span></h3>
-            <h3>P2: <span>00</span></h3>
+            <h3 className={`${player === 1 && "bg-green-500"}`}>P1: <span>00</span></h3>
+            <h3 className={`${player === 2 && "bg-green-500"}`}>P2: <span>00</span></h3>
         </div>
     )
 }
